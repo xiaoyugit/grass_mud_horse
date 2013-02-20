63ed9515c80ba5c78973e1e81f9d932908b51fbe
xiaoyugit <xiaoyu.seu@gmail.com>
2013-02-20 15:39:07 +0900

    tidy train.py and stop using location raw as feature input

--- benchmark/train.py
+++ benchmark/train.py
@@ -8,33 +8,38 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 
-def feature_extractor(data):
-    features = [('FullDescription-Bag of Words', 'FullDescription', CountVectorizer(max_features=200)),
-                ('Title-Bag of Words', 'Title', CountVectorizer(max_features=100)),
-                ('LocationRaw-Bag of Words', 'LocationRaw', CountVectorizer(max_features=100)),
-                ('LocationNormalized-Bag of Words', 'LocationNormalized', CountVectorizer(max_features=100)),
-                ('Category-Bag of Words','Category',CountVectorizer(max_features=100)),
-                ('Company-Bag of Words','Company',CountVectorizer(max_features=100)),
-                ('ContractType-Bag of Words','ContractType',CountVectorizer(tokenizer=my_tokenizer,min_df=1)),
-                ('ContractTime-Bag of Words','ContractTime',CountVectorizer(tokenizer=my_tokenizer,min_df=1)),
-                ('Source-Bag of Words','SourceName',CountVectorizer(tokenizer=my_tokenizer,min_df=1))]
-    combined = FeatureMapper(features)
-    return combined
-
 #callable that provides tokens for Company, ContractType, ContractTime and Source
 def my_tokenizer(s):
-    return s.split('?')
+    return s.strip().split('?')
 
+def my_vectorizer(type):
+    box = {"text": CountVectorizer(max_features = 200),
+            "category": CountVectorizer(tokenizer = my_tokenizer, min_df = 1)
+            }
+    return box[type]
+
+def feature_extractor(data):
+    features = [('FullDescription-Bag of Words', 'FullDescription', my_vectorizer("text")),
+                ('Title-Bag of Words', 'Title', my_vectorizer("text")),
+                #('LocationRaw-Bag of Words', 'LocationRaw', my_vectorizer("text")),
+                ('LocationNormalized-Bag of Words', 'LocationNormalized', my_vectorizer("category")),
+                ('Category-Bag of Words','Category',my_vectorizer("category")),
+                ('Company-Bag of Words','Company',my_vectorizer("category")),
+                ('ContractType-Bag of Words','ContractType',my_vectorizer("category")),
+                ('ContractTime-Bag of Words','ContractTime',my_vectorizer("category")),
+                ('Source-Bag of Words','SourceName',my_vectorizer("category"))]
+    combined = FeatureMapper(features)
+    return combined
 
 def get_pipeline(data):
     features = feature_extractor(data)
     steps = [("extract_features", features),
-             #("classify", RandomForestRegressor(n_estimators=50, 
-             #                                   verbose=2,
-             #                                   n_jobs=3,
-             #                                   min_samples_split=30,
-             #                                   random_state=3465343))]
-             ("classify", SVC())]
+             ("classify", RandomForestRegressor(n_estimators=50, 
+                                                verbose=2,
+                                                n_jobs=3,
+                                                min_samples_split=30,
+                                                random_state=3465343))]
+             #("classify", SVC())]
              #("classify", LinearRegression())]
     return Pipeline(steps)
 
